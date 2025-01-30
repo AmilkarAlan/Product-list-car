21d515685d4d42ce7a9b0ce2ead9ed5d6f18eab9
Amilkar Sanchez <amilkaralan@gmail.com>
2025-01-30 14:28:35 -0600

    Cart maquetado

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,14 +1,28 @@
 import { useCart } from '../AppContext'
-
+import empyLogo from "../assets/images/illustration-empty-cart.svg"
 const Cart = () => {
-  const {cart} = useCart()
+  const { cart } = useCart()
   return (
     <div className='w-full h-full bg-white p-4 rounded-2xl'>
-        <h5 className='text-3xl font-bold mb-8 text-(--color-red) '>Your Cart ({cart.items.length})</h5>
-        {cart.items.map((item)=> (
-          <p>{item.name} <span>{item.quantity}</span></p>
-        ))}
-        <h3>Total: ${cart.total.toFixed(2)}</h3>
+      <h5 className='text-3xl font-bold mb-8 text-(--color-red) '>Your Cart ({cart.items.length})</h5>
+      {cart.items.length === 0 ?
+        (
+          <div>
+            <img src={empyLogo} alt="" />
+            <p>Your added items will appear here</p>
+          </div>
+        ) :
+        cart.items.map((item) => (
+          <div key={item.id}>
+            <div>
+              <p>{item.name}</p>
+              <p><span>{item.quantity}x</span> <span>@ ${item.price}</span> <span>${item.total}</span></p>
+              <button>X</button>
+            </div>
+            <h3>Total: ${cart.total.toFixed(2)}</h3>
+          </div>
+        ))
+      }
     </div>
   )
 }
